fad89d8703bb7bead980379efb23c3c45a630231
Sennin <namle1998@hotmail.com>
2020-11-17 12:45:52 -0800

    Changed API call to anilist.co, created graphql query for anilist media, created get function for query

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,46 @@
 import React from 'react'
 
 import Head from 'next/head';
+import { getAllPostsForHome } from '../lib/api/api.js'
 
 import Container from '../components/Container';
 
-export default function Home({data}) {
+export default function Home({data, anilist}) {
+  console.log(anilist);
+  //console.log(data);
   return (
     <>
       <Head>
           <title>Learning nextjs</title>
       </Head>
       {
-        data.anime.map(ani => (
-          <Container name={ani.title} image={ani.image_url}/>
-        ))
+        anilist.Page.media.map(show => (
+          <Container name={show.title.romaji} />
+          ))
       }
     </>
   )
 }
 
-export async function getStaticProps(context) {
-  try {
-      const response = await fetch(`https://api.jikan.moe/v3/season/2020/fall`);
-      const data = await response.json();
-      return {
-          props: {
-              data
-          }
-      };
-  } catch (err) {
-      console.log(err);
-  };
+// export async function getStaticProps(context) {
+//   try {
+//       const response = await fetch(`https://api.jikan.moe/v3/season/2020/fall`);
+//       const data = await response.json();
+//       return {
+//           props: {
+//               data
+//           }
+//       };
+//   } catch (err) {
+//       console.log(err);
+//   };
+// }
+
+export async function getStaticProps() {
+  const anilist = (await getAllPostsForHome())
+  return {
+    props: {
+      anilist 
+    }
+  }
 }
