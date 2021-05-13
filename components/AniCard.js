1896b2fe4f83f27c8c169411a0dcda02e66239b2
aznamle <namle1998@hotmail.com>
2021-05-13 11:19:49 -0700

    package updates, minor update to query and components

--- components/AniCard.js
+++ components/AniCard.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function AniCard({ name, image, description}) {
     return (
-        <div className="mx-auto lg:min-w-full lg:flex lg:flex-row lg:h-full md:w-full mx-auto shadow-md hover:shadow-2xl transition duration-500">
-            <img className='rounded tr-md rounded-tl-md h-full w-full lg:h-auto lg:w-2/5 rounded-bl-md lg:rounded-br '
+        <div className="mx-auto lg:flex lg:h-full max-w-screen-2xl shadow-md hover:shadow-2xl transition duration-500">
+            <img className='rounded tr-md rounded-tl-md h-25 w-full  lg:w-1/5 rounded-bl-md lg:rounded-br '
                 src={image} alt='' 
             />
-            <div className='mx-auto lg:w-full lg:h-full bg-white p-4 rounded-br-md rounded-bl-md bg-white'>
+            <div className='mx-auto lg:w-full lg:h-full bg-white p-4 rounded-br-md rounded-bl-md w-4/5'>
                 <h2 className='text-gray-700 font-semibold p-2 truncate...'>{name}</h2>
                 <p className='text-sm text-gray-600 mt-1 p-2 overflow-y-auto h-28'>{description}</p>
             </div>
